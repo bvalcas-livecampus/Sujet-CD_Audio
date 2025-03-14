a45da57b11b47c2847a2c8eb18dd37a71fe2b7e2
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2025-03-14 15:19:27 +0100

    fix: span

--- client/src/components/CDItem.jsx
+++ client/src/components/CDItem.jsx
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 const CDItem = ({ cd, onDelete }) => {
   return (
     <li data-testid="cd-item">
-      <span data-testid="cd-title">{cd.title}</span> - 
-      <span data-testid="cd-artist">{cd.artist}</span> (
-      <span data-testid="cd-year">{cd.year}</span>)
+      <span>
+        <span data-testid="cd-title">{cd.title}</span>
+        <span> - </span> 
+        <span data-testid="cd-artist">{cd.artist}</span>
+        <span> (</span><span data-testid="cd-year">{cd.year}</span><span>)</span>
+      </span>
       <button className="delete-btn" onClick={() => onDelete(cd.id)}>🗑 Supprimer</button>
     </li>
   );
